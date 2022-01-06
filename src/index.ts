2facd8691431daab4d488a840e6528571750ee35
Katherine Yang <whykatherine@gmail.com>
2022-01-06 00:41:30 -0800

    feat: some fixes and tests

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,71 @@
 import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {SyntaxNode} from "@lezer/common"
+import {delimitedIndent, indentNodeProp, TreeIndentContext, LRLanguage, LanguageSupport} from "@codemirror/language"
 import {styleTags, tags as t} from "@codemirror/highlight"
+
+function indentBody(context: TreeIndentContext, node: SyntaxNode) {
+  let base = context.lineIndent(node.from)
+  let line = context.lineAt(context.pos, -1), to = line.from + line.text.length
+  // Don't consider blank, deindented lines at the end of the
+  // block part of the block
+  if (!/\S/.test(line.text) &&
+      context.node.to < to + 100 &&
+      !/\S/.test(context.state.sliceDoc(to, context.node.to)) &&
+      context.lineIndent(context.pos, -1) <= base)
+    return null
+  // A normally deindenting keyword that appears at a higher
+  // indentation than the block should probably be handled by the next
+  // level
+  if (/^\s*(else:|elif |except |finally:)/.test(context.textAfter) && context.lineIndent(context.pos, -1) > base)
+    return null
+  return base + context.unit
+}
 
 export const coemLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
+        Body: context => indentBody(context, context.node) ?? context.continue(),
+        IfStatement: cx => /^\s*(else )/.test(cx.textAfter) ? cx.baseIndent : cx.continue(),
+        "ParamList ArgList": delimitedIndent({closing: "—"}),
+        Script: context => {
+          if (context.pos + /\s*/.exec(context.textAfter)![0].length >= context.node.to) {
+            let endBody = null
+            for (let cur: SyntaxNode | null = context.node, to = cur.to;;) {
+              cur = cur.lastChild
+              if (!cur || cur.to != to) break
+              if (cur.type.name == "Body") endBody = cur
+            }
+            if (endBody) {
+              let bodyIndent = indentBody(context, endBody)
+              if (bodyIndent != null) return bodyIndent
+            }
+          }
+          return context.continue()
+        }
       }),
       styleTags({
-        Identifier: t.variableName,
+        "while if else": t.controlKeyword,
+        "not and or is am are": t.operatorKeyword,
+        "let to": t.definitionKeyword,
+        "print": t.keyword,
         Boolean: t.bool,
+        VariableName: t.variableName,
+        "CallExpression/VariableName": t.function(t.definition(t.variableName)),
+        "FunctionDefinition/VariableName": t.function(t.definition(t.variableName)),
+        Comment: t.lineComment,
         String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren,
-        "[ ]": t.squareBracket,
-        "— —": t.brace
+        "— —": t.bracket,
+        ",": t.separator
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    closeBrackets: {
+      brackets: ["(", "[", '"', "“", "—"]
+    },
+    commentTokens: {line: "†"},
+    indentOnInput: /^\s*([\}\]\)]|else)$/
   }
 })
 
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import {CoemLanguage} from "../dist/index.js"
+import {coemLanguage} from "../dist/index.js"
 import {fileTests} from "@lezer/generator/dist/test"
 
 import * as fs from "fs"
@@ -12,6 +12,6 @@ for (let file of fs.readdirSync(caseDir)) {
   let name = /^[^\.]*/.exec(file)[0]
   describe(name, () => {
     for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
-      it(name, () => run(CoemLanguage.parser))
+      it(name, () => run(coemLanguage.parser))
   })
 }
