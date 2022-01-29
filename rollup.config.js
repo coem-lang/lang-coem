a0fd24522132334f31263c9258c4f0520411c3f4
Katherine Yang <whykatherine@gmail.com>
2022-01-28 22:20:29 -0800

    feat: that was a bad idea, going back to excluding external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from "rollup-plugin-ts"
 import {lezer} from "@lezer/generator/rollup"
-import resolve from "@rollup/plugin-node-resolve"
 
 export default {
   input: "src/index.ts",
-  // external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
   output: {
     file: "dist/lang-coem.js"
   },
-  plugins: [lezer(), typescript(), resolve()]
+  plugins: [lezer(), typescript()]
 }
