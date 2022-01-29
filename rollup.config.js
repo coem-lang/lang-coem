24aa6c0c421178a1f860c5e4bc4836ca4814933b
Katherine Yang <whykatherine@gmail.com>
2022-01-28 21:48:33 -0800

    feat: try not exporting?

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import {lezer} from "@lezer/generator/rollup"
 export default {
   input: "src/index.ts",
   external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-  output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
-  ],
+  output: {
+    file: "dist/index.js"
+  },
   plugins: [lezer(), typescript()]
 }
